b2c6b22db69cf77c37da5ee7ef70bbcf4e99a5b2
Ahmed Belgacem <ahmedbelgaacem@gmail.com>
2021-12-09 19:42:54 +0100

    fixed pipeline

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages,setup
 setup(
     name='TunAugmentor',
     packages=find_packages(include=['TunAugmentor']),
-    version='0.1.2',
+    version='0.1.0',
     description='Python library for image data augmentation',
     author='Ahmed Belgacem, Firas Meddeb',
     author_email = 'ahmedbelgaacem@gmail.com',
