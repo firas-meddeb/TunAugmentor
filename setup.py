a8e88f84f2f9fe0b212b6fe910cfcfd5036da4da
Ahmed Belgacem <ahmedbelgaacem@gmail.com>
2021-12-09 19:07:36 +0100

    fixed classifiers

--- setup.py
+++ setup.py
@@ -8,16 +8,15 @@ setup(
     author='Ahmed Belgacem, Firas Meddeb',
     author_email = 'ahmedbelgaacem@gmail.com',
     url="https://github.com/ahmedbelgacem/TunAugmentor",
+    download_url ="https://github.com/ahmedbelgacem/TunAugmentor/archive/refs/tags/v_01.tar.gz",
     license='MIT',
-    install_requires=['numpy,opencv-python'],
+    install_requires=['numpy','opencv-python'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest==4.4.1'],
     test_suite='tests',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-        'Topic :: Data Science :: Image Augmentation',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.9.7',
+        'License :: OSI Approved :: MIT License'
     ],
 )
