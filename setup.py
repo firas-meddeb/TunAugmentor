317676e5cf1afa8d8c1f8aa0cd19d0009b88c9b9
Ahmed Belgacem <ahmedbelgaacem@gmail.com>
2021-12-09 11:54:17 +0100

    testing package upload

--- setup.py
+++ setup.py
@@ -6,9 +6,18 @@ setup(
     version='0.1.0',
     description='Python library for image data augmentation',
     author='Ahmed Belgacem, Firas Meddeb',
+    author_email = 'ahmedbelgaacem@gmail.com',
+    url="https://github.com/ahmedbelgacem/TunAugmentor",
     license='MIT',
-    install_requires=['numpy'],
+    install_requires=['numpy,opencv-python'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest==4.4.1'],
-    test_suite='tests'
+    test_suite='tests',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Data Science :: Image Augmentation',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.9.7',
+    ],
 )
